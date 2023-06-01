c39c370534fd2f8000e94cc5a584fa42ed596e23
sicheng1806 <sicheng1806@outlook.com>
2023-06-01 09:57:38 +0800

    更新了一个字符串分割工具

---
+++ __collections/__init__.py
@@ -0,0 +1,1 @@
+from __collections._str_ import StrTools
---
+++ __collections/_str_.py
@@ -0,0 +1,40 @@
+from collections.abc import Iterable
+
+
+class StrTools:
+
+    @staticmethod
+    def split_index(string:str,iterable:Iterable[int]) -> list[str]:
+        '''按索引分割字符串'''
+        def is_valid(iterable:Iterable[int]):
+            iterable = list(iterable)
+            if iterable[0] != 0:
+                iterable.insert(0,0)
+            for i in range(1,len(iterable)):
+                if not isinstance(iterable[i],int):raise TypeError("note::Iterable[int]")
+                if iterable[i-1] > iterable[i]:raise TypeError("note::iterable must increase")
+            return iterable
+        if not isinstance(string,str):raise TypeError("note::str")
+        iterable = is_valid(iterable)
+        str_lst = []
+        for i in range(len(iterable)):
+            if i == len(iterable)-1: 
+                str_lst.append(string[iterable[i]:])
+                continue
+            str_lst.append(string[iterable[i]:iterable[i+1]])
+        return str_lst
+        
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print(StrTools.split_index("HelloWorldComeToMySchool",[0,5,10,14,16]))
